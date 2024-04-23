db3641503fd457b2854f34bd27d0b10cfafd7b15
ilSimion <nsimion@fermimn.edu.it>
2024-04-23 12:49:06 +0200

    Update Custom to extend View

--- app/src/main/java/it/edu/fermimn/penaltykicks/Custom.java
+++ app/src/main/java/it/edu/fermimn/penaltykicks/Custom.java
@@ -1,12 +1,36 @@
 package it.edu.fermimn.penaltykicks;
 
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Canvas;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.View;
 
-public class Custom extends Activity {
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+
+public class Custom extends View {
+
+    public Custom(Context context) {
+        super(context);
+    }
+
+    public Custom(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public Custom(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public Custom(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
     @Override
-    public void onCreate(Bundle bundle) {
-        super.onCreate(bundle);
-        setContentView(R.layout.activity_custom);
+    protected void onDraw(@NonNull Canvas canvas) {
+        super.onDraw(canvas);
     }
 }
