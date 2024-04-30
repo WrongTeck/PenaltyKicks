4bb76bce789b2069e74cd01a9493850bf99661af
AymanAzim <azim.ayman04@gmail.com>
2024-04-30 11:23:27 +0200

    Modifiche del camu, creazione della classe Graficagioco

---
+++ app/src/main/java/it/edu/fermimn/penaltykicks/GraficaGioco.java
@@ -0,0 +1,61 @@
+package it.edu.fermimn.penaltykicks;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+
+public class GraficaGioco extends View {
+
+    int xpalla, ypalla, xportiere, yportiere;
+
+    private Paint paint;
+    private Bitmap imgPalla;
+    private Bitmap imgPortiere;
+    private Bitmap imgPorta;
+
+    private void inizializza() {
+        paint = new Paint();  // possiamo pensare all'oggetto paint come al "pennello" che ci server per disegnare
+        imgPorta = BitmapFactory.decodeResource(getResources(), R.drawable.porta_nobg_downscaled );
+        imgPortiere = BitmapFactory.decodeResource(getResources(), R.drawable.portiere_sprite1 );
+        imgPalla = BitmapFactory.decodeResource(getResources(), R.drawable.ball_sprite );
+    }
+
+    public GraficaGioco(Context context) {
+        super(context);
+        inizializza();
+    }
+
+    public GraficaGioco(Context context, @Nullable AttributeSet attrs) {
+
+        super(context, attrs);
+        inizializza();
+    }
+
+    public GraficaGioco(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        inizializza();
+    }
+
+    public GraficaGioco(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        inizializza();
+    }
+
+    @Override
+    protected void onDraw(@NonNull Canvas canvas) {
+        super.onDraw(canvas);
+
+        canvas.drawBitmap(imgPorta,0,0,paint);
+        canvas.drawBitmap(imgPalla,0,0,paint);
+        canvas.drawBitmap(imgPortiere,0,0,paint);
+
+    }
+}
